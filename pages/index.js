897dde64e1a479a2c3a46963a634815c06aa04c5
Daniel dos Santos Araujo <denenewton@icloud.com>
2023-05-24 16:39:39 -0300

    first-commit

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,18 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import styles from '../styles/Home.module.css';
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Learning Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Read <Link href='/posts/first-post' >Next.js!</Link>
         </h1>
 
         <p className={styles.description}>
